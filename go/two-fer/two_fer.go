0c77ce993ce6daeb9a653f5f74a58456d08eab08
Andreas <alorentson@flexera.com>
2020-03-04 13:33:43 -0800

    Go twofer exercise

---
+++ go/two-fer/two_fer.go
@@ -0,0 +1,12 @@
+// Package twofer describes two-for-one deals
+package twofer
+
+// ShareWith outputs the name of someone to share the twofer deal with
+// If blank will assume "you"
+// Assumes the sharer is included
+func ShareWith(name string) string {
+	if name == ""  {
+		name = "you"
+	}
+	return "One for " + name + ", one for me."
+}
