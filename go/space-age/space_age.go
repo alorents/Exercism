aa115ba6605872be665eec1559a5a46d7c6fda2d
Andreas <alorentson@flexera.com>
2020-03-04 12:50:11 -0800

    Eliminate unnecessary repetition

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -4,16 +4,16 @@ type Planet string
 
 const secondsInEarthYears = 31557600
 var secondsInPlanetYears = map[Planet]float64{
-	"Mercury":	secondsInEarthYears * 0.2408467,
-	"Venus": 	secondsInEarthYears * 0.61519726,
-	"Earth": 	secondsInEarthYears,
-	"Mars": 	secondsInEarthYears * 1.8808158,
-	"Jupiter": 	secondsInEarthYears * 11.862615,
-	"Saturn": 	secondsInEarthYears * 29.447498,
-	"Uranus": 	secondsInEarthYears * 84.016846,
-	"Neptune": 	secondsInEarthYears * 164.79132,
+	"Mercury":	 0.2408467,
+	"Venus": 	 0.61519726,
+	"Earth":	 1.0,
+	"Mars": 	 1.8808158,
+	"Jupiter": 	 11.862615,
+	"Saturn": 	 29.447498,
+	"Uranus": 	 84.016846,
+	"Neptune": 	 164.79132,
 }
 
 func Age(ageInSeconds float64, planet Planet) float64 {
-	return ageInSeconds / secondsInPlanetYears[planet]
+	return ageInSeconds / (secondsInEarthYears * secondsInPlanetYears[planet])
 }
