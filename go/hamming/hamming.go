a6d40788f32487469fa8b6361de92c62dc57e25a
Andreas <alorentson@flexera.com>
2020-03-31 15:40:40 -0700

    Use rune array to preserve character encoding

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -4,13 +4,15 @@ import "errors"
 
 // Distance calculates the Hamming Distance between two DNA strands.
 func Distance(a, b string) (int, error) {
-	if len(a) != len(b) {
+	ar, br := []rune(a), []rune(b)
+
+	if len(ar) != len(br) {
 		return 0, errors.New("sequences are of unequal length")
 	}
 
 	hammingDistance := 0
-	for pos := range a {
-		if a[pos] != b[pos] {
+	for pos := range ar {
+		if ar[pos] != br[pos] {
 			hammingDistance++
 		}
 	}
