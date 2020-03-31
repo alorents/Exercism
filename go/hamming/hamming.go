d61e8b5260eef1396f7a2cf4739821e5f5dfb18d
Andreas <alorentson@flexera.com>
2020-03-30 20:14:34 -0700

    Linting modifications

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -2,14 +2,14 @@ package hamming
 
 import "errors"
 
+// Distance calculates the Hamming Distance between two DNA strands.
 func Distance(a, b string) (int, error) {
-	hammingDistance := 0
-
 	if len(a) != len(b) {
-		return -1, errors.New("sequences are of unequal length")
+		return 0, errors.New("sequences are of unequal length")
 	}
 
-	for pos, _ := range a {
+	hammingDistance := 0
+	for pos := range a {
 		if a[pos] != b[pos] {
 			hammingDistance++
 		}
