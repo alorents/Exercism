4b792b8597071c151a1ab1a9a1c580ce93e348f1
Andreas <alorentson@flexera.com>
2020-05-12 13:30:24 -0700

    Use switch statement over map for better benchmark

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -4,48 +4,28 @@ import (
 	"unicode"
 )
 
-var (
-	letterValues = map[rune]int{
-		'A': 1,
-		'E': 1,
-		'I': 1,
-		'O': 1,
-		'U': 1,
-		'L': 1,
-		'N': 1,
-		'R': 1,
-		'S': 1,
-		'T': 1,
-
-		'D': 2,
-		'G': 2,
-
-		'B': 3,
-		'C': 3,
-		'M': 3,
-		'P': 3,
-
-		'F': 4,
-		'H': 4,
-		'V': 4,
-		'W': 4,
-		'Y': 4,
-
-		'K': 5,
-
-		'J': 8,
-		'X': 8,
-
-		'Q': 10,
-		'Z': 10,
-	}
-)
-
 // Score computes the scrabble score of a given word according to the values defined in letterValues
 func Score(input string) int {
 	totalScore := 0
+	letterValue := 0
 	for _, letter := range input {
-		letterValue := letterValues[unicode.ToUpper(letter)]
+		switch unicode.ToUpper(letter) {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+			letterValue = 1
+		case 'D', 'G':
+			letterValue = 2
+		case 'B', 'C', 'M', 'P':
+			letterValue = 3
+		case 'F', 'H', 'V', 'W', 'Y':
+			letterValue = 4
+		case 'K':
+			letterValue = 5
+		case 'J', 'X':
+			letterValue = 8
+		case 'Q', 'Z':
+			letterValue = 10
+		}
+
 		totalScore += letterValue
 	}
 	return totalScore
