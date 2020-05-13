cbc621cee59f143ee5e613d54871a5572caff3fe
Andreas <alorentson@flexera.com>
2020-05-13 09:35:32 -0700

    Remove unnecessary variable

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -7,26 +7,23 @@ import (
 // Score computes the scrabble score of a given word according to the values defined in letterValues
 func Score(input string) int {
 	totalScore := 0
-	letterValue := 0
 	for _, letter := range input {
 		switch unicode.ToUpper(letter) {
 		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
-			letterValue = 1
+			totalScore += 1
 		case 'D', 'G':
-			letterValue = 2
+			totalScore += 2
 		case 'B', 'C', 'M', 'P':
-			letterValue = 3
+			totalScore += 3
 		case 'F', 'H', 'V', 'W', 'Y':
-			letterValue = 4
+			totalScore += 4
 		case 'K':
-			letterValue = 5
+			totalScore += 5
 		case 'J', 'X':
-			letterValue = 8
+			totalScore += 8
 		case 'Q', 'Z':
-			letterValue = 10
+			totalScore += 10
 		}
-
-		totalScore += letterValue
 	}
 	return totalScore
 }
