9f4d45f0bbee9faa21c994a0153416dec5dd3c74
Andreas <alorentson@flexera.com>
2020-05-20 12:09:01 -0700

    Use named return value

--- go/accumulate/accumulate.go
+++ go/accumulate/accumulate.go
@@ -1,10 +1,10 @@
 package accumulate
 
 // Accumulate preforms the converter function on the given string array
-func Accumulate(given []string, converter func(string) string) []string {
-	output := make([]string, len(given))
+func Accumulate(given []string, converter func(string) string) (output []string) {
+	output = make([]string, len(given))
 	for i, s := range given {
 		output[i] = converter(s)
 	}
-	return output
+	return
 }
