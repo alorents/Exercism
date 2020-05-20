6c726ae9aab5e4f981bae641604f974599ba1b4c
Andreas <alorentson@flexera.com>
2020-05-19 23:15:51 -0700

    Initial solution for nucleotide go exercise

--- go/nucleotide-count/nucleotide_count.go
+++ go/nucleotide-count/nucleotide_count.go
@@ -1,11 +1,17 @@
 package dna
 
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
 // Histogram is a mapping from nucleotide to its count in given DNA.
 // Choose a suitable data type.
-type Histogram
+type Histogram map[rune]int
 
 // DNA is a list of nucleotides. Choose a suitable data type.
-type DNA
+type DNA []rune
 
 // Counts generates a histogram of valid nucleotides in the given DNA.
 // Returns an error if d contains an invalid nucleotide.
@@ -14,6 +20,19 @@ type DNA
 // The receiver appears in its own argument list between the func keyword and the method name.
 // Here, the Counts method has a receiver of type DNA named d.
 func (d DNA) Counts() (Histogram, error) {
-	var h Histogram
+	var h = Histogram{
+		'A': 0,
+		'C': 0,
+		'G': 0,
+		'T': 0,
+	}
+
+	for _, nucleotide := range d {
+		if strings.ContainsRune("ACGT", nucleotide) {
+			h[nucleotide]++
+		} else {
+			return nil, errors.New(fmt.Sprintf("Invalid nucleotide: %q", nucleotide))
+		}
+	}
 	return h, nil
 }
