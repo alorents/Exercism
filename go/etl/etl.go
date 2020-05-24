1cc72b3048778250ad2df9ab6c903f0e2411eea5
Andreas <alorentson@flexera.com>
2020-05-24 01:23:41 -0700

    add function comment

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 )
 
+// Transforms a legacy scrabble score valueToLetter map to a letterToValue map
 func Transform(valueToLetter map[int][]string) map[string]int {
 	letterToValue := make(map[string]int)
 	for value, letterArray := range valueToLetter {
