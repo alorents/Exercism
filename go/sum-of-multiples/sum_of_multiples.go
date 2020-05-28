4ee1a997ddeec1997007364ab99ac7104343fd64
Andreas <alorentson@flexera.com>
2020-05-28 15:47:42 -0700

    slightly faster solution

--- go/sum-of-multiples/sum_of_multiples.go
+++ go/sum-of-multiples/sum_of_multiples.go
@@ -2,24 +2,13 @@ package summultiples
 
 // SumMultiples return the sum of all unique multiples matching the given divisors under a limit
 func SumMultiples(limit int, divisors ...int) int {
-	if limit < 2 {
-		return 0
-	}
 	sum := 0
 	for i := 1; i < limit; i++ {
-		isMultiple := func() bool {
-			for _, divisor := range divisors {
-				if divisor == 0 {
-					continue
-				}
-				if i%divisor == 0 {
-					return true
-				}
+		for _, divisor := range divisors {
+			if divisor > 0 && i%divisor == 0 {
+				sum += i
+				break
 			}
-			return false
-		}
-		if isMultiple() {
-			sum += i
 		}
 	}
 	return sum
