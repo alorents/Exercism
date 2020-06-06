c78fe1129b7abfa28e9a99d058653e627d03349b
Andreas <alorentson@flexera.com>
2020-06-05 19:43:44 -0700

    solution with strings.Map

--- go/crypto-square/crypto_square.go
+++ go/crypto-square/crypto_square.go
@@ -6,36 +6,18 @@ import (
 	"unicode"
 )
 
+// Encode uses the crypto-square method to encode plaintext
 func Encode(plaintext string) string {
-	if len(plaintext) < 1 {
-		return ""
+	if len(plaintext) < 2 {
+		return plaintext
 	}
-	replacer := strings.NewReplacer(
-		" ", "",
-		"!", "",
-		"@", "",
-		"#", "",
-		"$", "",
-		"%", "",
-		"^", "",
-		"&", "",
-		"*", "",
-		"(", "",
-		")", "",
-		",", "",
-		".", "",
-	)
-	text := replacer.Replace(plaintext)
-	rows := int(math.Sqrt(float64(len(text))))
-	columns := rows
-	if rows*columns < len(text) {
-		columns++
-		if rows*columns < len(text) {
-			rows++
-		}
-	}
+	text := normalizeText(plaintext)
+	rows, columns := calculateDimensions(len(text))
+	// add padding if needed to make a perfect rectangle
 	paddingSize := rows*columns - len(text)
 	text += strings.Repeat(" ", paddingSize)
+
+	// build encoded text "column" x "row"
 	var builder strings.Builder
 	rowIndex, columnIndex := 0, 0
 	for i := 0; i < len(text); i++ {
@@ -51,4 +33,28 @@ func Encode(plaintext string) string {
 		}
 	}
 	return builder.String()
+}
+
+// normalize text
+func normalizeText(plaintext string) string {
+	text := strings.Map(func(r rune) rune {
+		if !unicode.IsLetter(r) && !unicode.IsDigit(r) {
+			return -1
+		}
+		return unicode.ToLower(r)
+	}, plaintext)
+	return text
+}
+
+// calculateDimensions determines the correct row and column size
+func calculateDimensions(length int) (int, int) {
+	rows := int(math.Sqrt(float64(length)))
+	columns := rows
+	if rows*columns < length {
+		columns++
+		if rows*columns < length {
+			rows++
+		}
+	}
+	return rows, columns
 }
