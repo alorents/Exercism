2602111dc3154c0adfeb2079059a1f5e3b8448ab
Andreas <alorentson@flexera.com>
2020-06-11 14:20:30 -0700

    Mentor feedback

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -16,12 +16,15 @@ func Square(square int) (uint64, error) {
 	if square < minSquare || square > maxSquare {
 		return 0, errors.New("input must match a square on a chessboard")
 	}
-	return uint64(1 << (square-1)), nil
+	return 1 << (square - 1), nil
 }
 
 // Total calculates the total number of grains on the board following the premise described in the square function
 // the sum of 2^0 + 2^1 + 2^2... + 2^n can be expressed as: x = 2^(n+1)-1
 func Total() uint64 {
-	total, _ := Square(maxSquare)
+	total, err := Square(maxSquare)
+	if err != nil {
+		return 0
+	}
 	return total*2 - 1
 }
