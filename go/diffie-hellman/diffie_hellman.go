b0cc76a281e8d7802feba9e298c3a2e07bc31b31
Andreas <alorentson@flexera.com>
2020-11-03 17:20:41 -0800

    diffie helman initial solution

--- go/diffie-hellman/diffie_hellman.go
+++ go/diffie-hellman/diffie_hellman.go
@@ -1,1 +1,45 @@
 package diffiehellman
+
+import (
+	"crypto/rand"
+	"math/big"
+)
+
+// PrivateKey generates a random key with value greater than 1 and less than p
+func PrivateKey(p *big.Int) *big.Int {
+	_one := big.NewInt(1)
+	privateKey := _one
+	for privateKey.Cmp(_one) <= 0 {
+		var err error
+		privateKey, err = rand.Int(rand.Reader, p)
+		if err != nil {
+			return big.NewInt(0)
+		}
+	}
+
+	return privateKey
+}
+
+// PublicKey calulates a public key = g**a mod p
+func PublicKey(a, p *big.Int, g int64) *big.Int {
+	_one := big.NewInt(1)
+	publicKey := _one
+	publicKey.Exp(big.NewInt(g), a, p)
+	return publicKey
+}
+
+// SecretKey calculates a secret key = B**a mod p
+func SecretKey(a, B, p *big.Int) *big.Int {
+	_one := big.NewInt(1)
+	secretKey := _one
+	secretKey.Exp(B, a, p)
+
+	return secretKey
+}
+
+// NewPair generates a new private key and public key from provided p, and g values
+func NewPair(p *big.Int, g int64) (*big.Int, *big.Int) {
+	a := PrivateKey(p)
+	A := PublicKey(a, p, g)
+	return a, A
+}
