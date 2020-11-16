0929e9fae515b138295f8f9ccfde0a68d35048bf
Andreas <alorentson@flexera.com>
2020-11-16 15:57:47 -0800

    simple-cipher implement VigenereCipher
    - decode by encoding with a flag, probably not ideal solution, mostly
      just wanted to play with it a bit

--- go/simple-cipher/simple_cipher.go
+++ go/simple-cipher/simple_cipher.go
@@ -1,38 +1,70 @@
 package cipher
 
 import (
+	"regexp"
 	"strings"
+	"unicode"
 )
 
-type ShiftCipher int32
-type VigenereCipher string
+// VigenereCipher contains the key for a Vigenere Cipher 
+// encode bool controls the direction(encode/decode) of the shift when applying key
+type VigenereCipher struct {
+	key string
+	encode bool
+}
 
+// NewCaesar creates a specific implementation of a Shift Cipher
 func NewCaesar() Cipher {
 	return NewShift(3)
 }
 
+// NewShift creates a Shift Cipher from a provide shift distance by implementing a simplified Vigenere Cipher
 func NewShift(distance int) Cipher {
 	if distance == 0 || distance <= -26 || distance >= 26 {
 		return nil
 	}
-	return ShiftCipher(distance)
+	return VigenereCipher{string('a' + distance), true}
 }
 
+// NewVigenere crates a Vigenere Cipher from a provided key
 func NewVigenere(key string) Cipher {
-	var cipher Cipher
-	return cipher
+	isValidKey := false
+	for _, r := range key {
+		if r == 'a' {
+			continue
+		}
+		if !unicode.IsLower(r) {
+			return nil
+		}
+		isValidKey = true
+	}
+	if !isValidKey {
+		return nil
+	}
+
+	return VigenereCipher{key, true}
 }
 
-func (cipher ShiftCipher) Encode(plaintText string) string {
-	distance := int32(cipher)
-	if distance < 0 {
-		distance += 26
-	}
-	plaintText = strings.ToLower(plaintText)
+// NewDecodeVigenere creates a decoding Vigenere Cipher by applying the provided key backwards.
+// Reverse compatible with Shit and Ceaser Ciphers
+func NewDecodeVigenere(key string) Cipher {
+	return VigenereCipher{key, false}
+}
+
+// Encode applies the key in the direction specified by VigenereCipher
+func (cipher VigenereCipher) Encode(plaintText string) string {
+	normailizedText := regexp.MustCompile(`[^a-z]`).ReplaceAllString(strings.ToLower(plaintText), "")
 	encoded := ""
-	for _, r := range plaintText {
+	for i, r := range normailizedText {
 		if r < 'a' || r > 'z' {
 			continue
+		}
+		distance := int32(cipher.key[i%len(cipher.key)]) - 'a'
+		if !cipher.encode {
+			distance *= -1
+		}
+		if distance < 0 {
+			distance += 26
 		}
 		encodedRune := r + distance
 		if encodedRune > 'z' {
@@ -44,8 +76,9 @@ func (cipher ShiftCipher) Encode(plaintText string) string {
 	return encoded
 }
 
-func (cipher ShiftCipher) Decode(plaintText string) string {
-	decode := NewShift(-int(cipher))
+// Decode decodes the encoded text by applying the specified cipher key in reverse
+func (cipher VigenereCipher) Decode(encodedText string) string {
+	decode := NewDecodeVigenere(cipher.key)
 
-	return decode.Encode(plaintText)
+	return decode.Encode(encodedText)
 }
