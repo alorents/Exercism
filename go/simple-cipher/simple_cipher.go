efbbb728949f9786f5bef641064dd82d7abe332d
Andreas <alorentson@flexera.com>
2020-11-16 14:57:27 -0800

    simple-cipher ceaser and shift ciphers

--- go/simple-cipher/simple_cipher.go
+++ go/simple-cipher/simple_cipher.go
@@ -1,1 +1,51 @@
 package cipher
+
+import (
+	"strings"
+)
+
+type ShiftCipher int32
+type VigenereCipher string
+
+func NewCaesar() Cipher {
+	return NewShift(3)
+}
+
+func NewShift(distance int) Cipher {
+	if distance == 0 || distance <= -26 || distance >= 26 {
+		return nil
+	}
+	return ShiftCipher(distance)
+}
+
+func NewVigenere(key string) Cipher {
+	var cipher Cipher
+	return cipher
+}
+
+func (cipher ShiftCipher) Encode(plaintText string) string {
+	distance := int32(cipher)
+	if distance < 0 {
+		distance += 26
+	}
+	plaintText = strings.ToLower(plaintText)
+	encoded := ""
+	for _, r := range plaintText {
+		if r < 'a' || r > 'z' {
+			continue
+		}
+		encodedRune := r + distance
+		if encodedRune > 'z' {
+			encodedRune -= 26
+		}
+		encoded += string(encodedRune)
+	}
+
+	return encoded
+}
+
+func (cipher ShiftCipher) Decode(plaintText string) string {
+	decode := NewShift(-int(cipher))
+
+	return decode.Encode(plaintText)
+}
