7fbaed4bdd03acf615cb1f58d3662c56a1e02227
Andreas <alorentson@flexera.com>
2020-11-18 14:10:22 -0800

    secret-handshake exercise solution using binary literals and operators

--- go/secret-handshake/secret_handshake.go
+++ go/secret-handshake/secret_handshake.go
@@ -1,1 +1,27 @@
 package secret
+
+func Handshake(code uint) []string {
+	var handshake []string
+	if code&0b1 == 0b1 {
+		handshake = append(handshake, "wink")
+	}
+	if code&0b10 == 0b10 {
+		handshake = append(handshake, "double blink")
+	}
+	if code&0b100 == 0b100 {
+		handshake = append(handshake, "close your eyes")
+	}
+	if code&0b1000 == 0b1000 {
+		handshake = append(handshake, "jump")
+	}
+	if code&0b10000 == 0b10000 {
+		for i := 0; i < len(handshake)/2; i++ {
+			j := len(handshake) - i - 1
+			temp := handshake[i]
+			handshake[i] = handshake[j]
+			handshake[j] = temp
+		}
+	}
+
+	return handshake
+}
