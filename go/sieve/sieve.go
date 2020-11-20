d2be8e91ff58475e0438054ec4351f3411a1e41b
Andreas <alorentson@flexera.com>
2020-11-19 17:31:12 -0800

    sieve-of-eratosthenes exercise solution

--- go/sieve/sieve.go
+++ go/sieve/sieve.go
@@ -1,1 +1,20 @@
 package sieve
+
+func Sieve(limit int) []int {
+	if limit < 2 {
+		return []int{}
+	}
+	var primes []int
+	marked := map[int]bool{}
+	for i := 2; i <= limit; i++ {
+		_, ok := marked[i]
+		if !ok {
+			primes = append(primes, i)
+			for j := i; j <= limit; j += i {
+				marked[j] = true
+			}
+		}
+	}
+
+	return primes
+}
