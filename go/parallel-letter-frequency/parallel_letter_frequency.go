6e281451373bd5393e49ee2d730fc3125a044a5b
Andreas <alorentson@flexera.com>
2020-12-02 15:18:10 -0800

    parallel letter frequency go solution

--- go/parallel-letter-frequency/parallel_letter_frequency.go
+++ go/parallel-letter-frequency/parallel_letter_frequency.go
@@ -12,3 +12,23 @@ func Frequency(s string) FreqMap {
 	}
 	return m
 }
+
+// ConcurrentFrequency takes a []string and runs Frequency on each string concurrently
+func ConcurrentFrequency(texts []string) FreqMap {
+	freqMapChan := make(chan FreqMap)
+
+	for _, text := range texts {
+		go func(text string) {
+			freqMapChan <- Frequency(text)
+		}(text)
+	}
+
+	freqMap := FreqMap{}
+	for i := 0; i < len(texts); i++ {
+		for k, v := range <-freqMapChan {
+			freqMap[k] += v
+		}
+	}
+
+	return freqMap
+}
