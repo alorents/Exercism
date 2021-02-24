755d3494289fd5f442589a2fc7766ba9bf7a5daa
Andreas <alorentson@flexera.com>
2021-02-24 15:21:55 -0800

    simplified solution with map

--- go/allergies/allergies.go
+++ go/allergies/allergies.go
@@ -1,17 +1,15 @@
 package allergies
 
-/*
-Allergy score mapping:
-	eggs (1)
-	peanuts (2)
-	shellfish (4)
-	strawberries (8)
-	tomatoes (16)
-	chocolate (32)
-	pollen (64)
-	cats (128)
-*/
-var allergenList = []string{"eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"}
+var allergenList = map[string]uint{
+	"eggs":         1,
+	"peanuts":      2,
+	"shellfish":    4,
+	"strawberries": 8,
+	"tomatoes":     16,
+	"chocolate":    32,
+	"pollen":       64,
+	"cats":         128,
+}
 
 //Allergies returns a list of allergens based on a person's allergy score
 func Allergies(score uint) []string {
@@ -19,7 +17,7 @@ func Allergies(score uint) []string {
 	if score <= 0 {
 		return results
 	}
-	for _, substance := range allergenList {
+	for substance := range allergenList {
 		if AllergicTo(score, substance) == true {
 			results = append(results, substance)
 		}
@@ -32,23 +30,6 @@ func AllergicTo(score uint, substance string) bool {
 	if score <= 0 {
 		return false
 	}
-	switch substance {
-	case "eggs":
-		return score&1 == 1
-	case "peanuts":
-		return score&2 == 2
-	case "shellfish":
-		return score&4 == 4
-	case "strawberries":
-		return score&8 == 8
-	case "tomatoes":
-		return score&16 == 16
-	case "chocolate":
-		return score&32 == 32
-	case "pollen":
-		return score&64 == 64
-	case "cats":
-		return score&128 == 128
-	}
-	return true
+	var substanceScore = allergenList[substance]
+	return score&substanceScore == substanceScore
 }
