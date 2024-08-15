697ec711339d56c37d114b6e9e04bb85659b8466
Andreas Lorentson <andreasphoenix@gmail.com>
2024-08-15 15:48:39 -0700

    collatz solution

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,21 @@
 package collatzconjecture
 
+import "errors"
+
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	if n < 1 {
+		return 0, errors.New("n must be a positive integer")
+	}
+	count := 0
+	for {
+		if n == 1 {
+			return count, nil
+		}
+		if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = 3*n + 1
+		}
+		count++
+	}
 }
