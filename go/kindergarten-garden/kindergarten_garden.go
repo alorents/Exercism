f61c5f906d97147736e5eb653d27467a8520584b
Andreas Lorentson <andreasphoenix@gmail.com>
2024-09-16 14:03:29 -0700

    kindergarten solution

--- go/kindergarten-garden/kindergarten_garden.go
+++ go/kindergarten-garden/kindergarten_garden.go
@@ -1,6 +1,13 @@
 package kindergarten
 
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
 // Define the Garden type here.
+type Garden map[string][]string
 
 // The diagram argument starts each row with a '\n'.  This allows Go's
 // raw string literals to present diagrams in source code nicely as two
@@ -11,9 +18,46 @@ package kindergarten
 //     VVCCGG`
 
 func NewGarden(diagram string, children []string) (*Garden, error) {
-	panic("Please implement the NewGarden function")
+	garden := make(Garden)
+
+	rows := strings.Split(diagram, "\n")
+	if len(rows) != 3 || len(rows[1]) != len(rows[2]) || len(rows[1])%2 != 0 {
+		return nil, fmt.Errorf("unexpected diagram input")
+	}
+	if len(rows[1]) != 2*len(children) {
+		return nil, fmt.Errorf("diagram - children mismatch")
+	}
+
+	plantsLookup := map[rune]string{
+		'C': "clover",
+		'G': "grass",
+		'R': "radishes",
+		'V': "violets",
+	}
+
+	sortedChildren := make([]string, len(children))
+	copy(sortedChildren, children)
+	sort.Strings(sortedChildren)
+
+	for i, child := range sortedChildren {
+		p0, ok0 := plantsLookup[rune(rows[1][2*i])]
+		p1, ok1 := plantsLookup[rune(rows[1][2*i+1])]
+		p2, ok2 := plantsLookup[rune(rows[2][2*i])]
+		p3, ok3 := plantsLookup[rune(rows[2][2*i+1])]
+		if !(ok0 && ok1 && ok2 && ok3) {
+			return nil, fmt.Errorf("invalid plant code")
+		}
+		garden[child] = []string{p0, p1, p2, p3}
+	}
+
+	if len(garden) != len(sortedChildren) {
+		return nil, fmt.Errorf("duplicate child name")
+	}
+
+	return &garden, nil
 }
 
 func (g *Garden) Plants(child string) ([]string, bool) {
-	panic("Please implement the Plants function")
+	plants, ok := (*g)[child]
+	return plants, ok
 }
