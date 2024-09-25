61edf54f136af68f371ad68c038342e04c93595d
Andreas Lorentson <andreasphoenix@gmail.com>
2024-09-24 18:32:02 -0700

    flatten-array solution

--- go/flatten-array/flatten_array.go
+++ go/flatten-array/flatten_array.go
@@ -1,5 +1,17 @@
 package flatten
 
 func Flatten(nested interface{}) []interface{} {
-	panic("Please implement the Flatten function")
+	result := make([]interface{}, 0)
+
+	switch i := nested.(type) {
+	case []interface{}:
+		for _, val := range i {
+			result = append(result, Flatten(val)...)
+		}
+
+	case interface{}:
+		result = append(result, i)
+	}
+
+	return result
 }
