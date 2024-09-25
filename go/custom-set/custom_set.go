a98d34b4e7d1d03d38d56e6c09c152a25b211360
Andreas Lorentson <andreasphoenix@gmail.com>
2024-09-24 17:55:47 -0700

    custom-set solution

--- go/custom-set/custom_set.go
+++ go/custom-set/custom_set.go
@@ -1,5 +1,9 @@
 package stringset
 
+import (
+	"fmt"
+)
+
 // Implement Set as a collection of unique string values.
 //
 // For Set.String, use '{' and '}', output elements as double-quoted strings
@@ -8,51 +12,123 @@ package stringset
 // Format the empty set as {}.
 
 // Define the Set type here.
+type Set struct {
+	elements map[string]string
+}
 
 func New() Set {
-	panic("Please implement the New function")
+	return Set{
+		elements: make(map[string]string),
+	}
 }
 
 func NewFromSlice(l []string) Set {
-	panic("Please implement the NewFromSlice function")
+	set := New()
+	for _, element := range l {
+		set.Add(element)
+	}
+	return set
 }
 
 func (s Set) String() string {
-	panic("Please implement the String function")
+	elementString := ""
+	keys := make([]string, 0, len(s.elements))
+	for k := range s.elements {
+		keys = append(keys, k)
+	}
+	if len(keys) == 1 {
+		return fmt.Sprintf("{\"%s\"}", keys[0])
+	}
+	for i, key := range keys {
+		if i == 0 {
+			elementString += fmt.Sprintf("\"%s\"", key)
+		} else {
+			elementString += fmt.Sprintf(", \"%s\"", key)
+		}
+	}
+
+	return fmt.Sprintf("{%s}", elementString)
 }
 
 func (s Set) IsEmpty() bool {
-	panic("Please implement the IsEmpty function")
+	return len(s.elements) == 0
 }
 
 func (s Set) Has(elem string) bool {
-	panic("Please implement the Has function")
+	_, ok := s.elements[elem]
+	return ok
 }
 
 func (s Set) Add(elem string) {
-	panic("Please implement the Add function")
+	s.elements[elem] = elem
 }
 
 func Subset(s1, s2 Set) bool {
-	panic("Please implement the Subset function")
+	if len(s1.elements) > len(s2.elements) {
+		return false
+	}
+	for elem := range s1.elements {
+		if !s2.Has(elem) {
+			return false
+		}
+	}
+
+	return true
 }
 
 func Disjoint(s1, s2 Set) bool {
-	panic("Please implement the Disjoint function")
+	return len(Intersection(s1, s2).elements) == 0
 }
 
 func Equal(s1, s2 Set) bool {
-	panic("Please implement the Equal function")
+	if len(s1.elements) != len(s2.elements) {
+		return false
+	}
+	for elem := range s1.elements {
+		if !s2.Has(elem) {
+			return false
+		}
+	}
+	return true
 }
 
 func Intersection(s1, s2 Set) Set {
-	panic("Please implement the Intersection function")
+	s3 := New()
+
+	for elem := range s1.elements {
+		if s2.Has(elem) {
+			s3.Add(elem)
+		}
+	}
+	for elem := range s2.elements {
+		if s1.Has(elem) {
+			s3.Add(elem)
+		}
+	}
+
+	return s3
 }
 
 func Difference(s1, s2 Set) Set {
-	panic("Please implement the Difference function")
+	if s2.IsEmpty() {
+		return s1
+	}
+	s3 := New()
+	for elem := range s1.elements {
+		if !s2.Has(elem) {
+			s3.Add(elem)
+		}
+	}
+	return s3
 }
 
 func Union(s1, s2 Set) Set {
-	panic("Please implement the Union function")
+	s3 := New()
+	for elem := range s1.elements {
+		s3.Add(elem)
+	}
+	for elem := range s2.elements {
+		s3.Add(elem)
+	}
+	return s3
 }
