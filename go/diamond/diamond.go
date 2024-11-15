d47a22760b709b271088831c1718c26ed7e5d472
Andreas Lorentson <andreasphoenix@gmail.com>
2024-11-14 19:59:50 -0800

    initial solution

--- go/diamond/diamond.go
+++ go/diamond/diamond.go
@@ -1,5 +1,54 @@
 package diamond
 
+import (
+	"fmt"
+	"strings"
+)
+
 func Gen(char byte) (string, error) {
-	panic("Please implement the Gen function")
+	if char < 'A' || char > 'Z' {
+		return "", fmt.Errorf("invalid character, expected input A..Z")
+	}
+	if char == 'A' {
+		return "A", nil
+	}
+
+	var top string
+	var bottom string
+	outerSpaces := int(char - 'A')
+	innerSpaces := 0
+
+	for c := 'A'; c <= rune(char); c++ {
+		line := ""
+		for i := 0; i < outerSpaces; i++ {
+			line += " "
+		}
+		line += string(c)
+		if innerSpaces > 0 {
+			for i := 0; i < innerSpaces; i++ {
+				line += " "
+			}
+			line += string(c)
+		}
+		for i := 0; i < outerSpaces; i++ {
+			line += " "
+		}
+
+		outerSpaces--
+		if innerSpaces == 0 {
+			innerSpaces++
+		} else {
+			innerSpaces += 2
+		}
+		if c == rune(char) {
+			top += line + "\n"
+		} else {
+			top += line + "\n"
+			bottom = line + "\n" + bottom
+		}
+	}
+
+	s := top + bottom
+	s, _ = strings.CutSuffix(s, "\n")
+	return s, nil
 }
--- go/diamond/go.mod
+++ go/diamond/go.mod
@@ -1,3 +1,7 @@
 module diamond
 
-go 1.18
+go 1.22.0
+
+toolchain go1.23.0
+
+require golang.org/x/exp v0.0.0-20241108190413-2d47ceb2692f
