85fc2bb5e6213c9e29b93c7c71584f0e2b51b1b2
Andreas Lorentson <andreasphoenix@gmail.com>
2024-11-18 15:19:30 -0800

    solution

--- go/pascals-triangle/pascals_triangle.go
+++ go/pascals-triangle/pascals_triangle.go
@@ -1,5 +1,21 @@
 package pascal
 
 func Triangle(n int) [][]int {
-	panic("Please implement the Triangle function")
+	if n < 1 {
+		return [][]int{}
+	}
+	rows := make([][]int, n)
+	for i := 0; i < n; i++ {
+		row := make([]int, i+1)
+		for j := 0; j < i+1; j++ {
+			// left and right side have < 2 addends and are always 1
+			if j == 0 || j == i {
+				row[j] = 1
+			} else {
+				row[j] = rows[i-1][j-1] + rows[i-1][j]
+			}
+		}
+		rows[i] = row
+	}
+	return rows
 }
