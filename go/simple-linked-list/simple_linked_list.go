a46a1179ba8d7acc06448f6ca0c0be9fdbc23fc9
Andreas Lorentson <andreasphoenix@gmail.com>
2024-11-24 11:28:16 -0800

    solution

--- go/simple-linked-list/simple_linked_list.go
+++ go/simple-linked-list/simple_linked_list.go
@@ -1,27 +1,73 @@
 package linkedlist
 
+import "fmt"
+
 // Define the List and Element types here.
+type (
+	Element struct {
+		value int
+		next  *Element
+	}
+
+	List struct {
+		head *Element
+		size int
+	}
+)
 
 func New(elements []int) *List {
-	panic("Please implement the New function")
+	if len(elements) < 1 {
+		return &List{}
+	}
+
+	l := &List{nil, 0}
+	for _, element := range elements {
+		l.Push(element)
+	}
+	return l
 }
 
 func (l *List) Size() int {
-	panic("Please implement the Size function")
+	return l.size
 }
 
 func (l *List) Push(element int) {
-	panic("Please implement the Push function")
+	l.head = &Element{value: element, next: l.head}
+	l.size++
 }
 
 func (l *List) Pop() (int, error) {
-	panic("Please implement the Pop function")
+	if l.size < 1 {
+		return 0, fmt.Errorf("empty list")
+	}
+
+	value := l.head.value
+	l.head = l.head.next
+	l.size--
+
+	return value, nil
 }
 
 func (l *List) Array() []int {
-	panic("Please implement the Array function")
+	if l.size < 1 {
+		return []int{}
+	}
+	array := make([]int, l.size)
+	for i, ptr := l.size-1, l.head; ptr != nil; i, ptr = i-1, ptr.next {
+		array[i] = ptr.value
+	}
+	return array
 }
 
 func (l *List) Reverse() *List {
-	panic("Please implement the Reverse function")
+	rev := &List{}
+	if l.size < 1 {
+		return rev
+	}
+	ptr := l.head
+	for ptr != nil {
+		rev.Push(ptr.value)
+		ptr = ptr.next
+	}
+	return rev
 }
