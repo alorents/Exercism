0c342860f37b9b927c72a5ebb29c8ddb3a4f2e8e
Andreas Lorentson <andreasphoenix@gmail.com>
2024-11-25 14:39:53 -0800

    cleaner solution

--- go/binary-search/binary_search.go
+++ go/binary-search/binary_search.go
@@ -9,38 +9,20 @@ func SearchInts(list []int, key int) int {
 	if key < list[0] || key > list[len(list)-1] {
 		return -1
 	}
-	start := 0
-	end := len(list) - 1
-	mid := (start + end) / 2
 
-	for {
-		// found
-		if list[mid] == key {
+	for start, end := 0, len(list)-1; start <= end; {
+		mid := (start + end) / 2
+		switch {
+		case list[mid] == key:
+			// found
 			return mid
-		}
-		// narrowed to one option and not found
-		if start == end {
-			return -1
-		}
-		// in "bottom" half
-		if list[mid] > key {
-			// adjust for even sized slices without a true midpoint
-			if (mid-start)%2 == 1 {
-				end = mid - 1
-			} else {
-				end = mid
-			}
-			mid = (start + end) / 2
-		}
-		// in "top" half
-		if list[mid] < key {
-			// adjust for even sized slices without a true midpoint
-			if (end-mid)%2 == 1 {
-				start = mid + 1
-			} else {
-				start = mid
-			}
-			mid = (start + end) / 2
+		case list[mid] < key:
+			// search "top" half
+			start = mid + 1
+		case list[mid] > key:
+			// search "bottom" half
+			end = mid - 1
 		}
 	}
+	return -1
 }
