85e74b9b3b29c0a052fc05f94abc1d04d05841c7
Andreas Lorentson <andreasphoenix@gmail.com>
2024-12-03 19:54:51 -0800

    solution

--- go/transpose/transpose.go
+++ go/transpose/transpose.go
@@ -1,5 +1,33 @@
 package transpose
 
+import (
+	"strings"
+)
+
 func Transpose(input []string) []string {
-	panic("Please implement the Transpose function")
+	result := []string{}
+	if len(input) == 0 {
+		return result
+	}
+
+	max := 0
+	for _, row := range input {
+		if len(row) > max {
+			max = len(row)
+		}
+	}
+
+	for i := 0; i < max; i++ {
+		row := ""
+		for j := range input {
+			if i < len(input[j]) {
+				row += string(input[j][i])
+			} else {
+				row += "#"
+			}
+		}
+		result = append(result, strings.ReplaceAll(strings.TrimRight(row, "#"), "#", " "))
+	}
+
+	return result
 }
