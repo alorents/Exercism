849f1b0d8c2dbdadb15c3d0c507856a0ffac839a
Andreas Lorentson <andreasphoenix@gmail.com>
2024-12-05 14:08:42 -0800

    solution

--- go/circular-buffer/circular_buffer.go
+++ go/circular-buffer/circular_buffer.go
@@ -1,5 +1,7 @@
 package circular
 
+import "fmt"
+
 // Implement a circular buffer of bytes supporting both overflow-checked writes
 // and unconditional, possibly overwriting, writes.
 //
@@ -8,23 +10,63 @@ package circular
 // replacement for anything using that interface.
 
 // Define the Buffer type here.
+type Buffer struct {
+	buffer     []byte
+	readIndex  int
+	writeIndex int
+	size       int
+}
 
 func NewBuffer(size int) *Buffer {
-	panic("Please implement the NewBuffer function")
+	bytes := make([]byte, size)
+	return &Buffer{buffer: bytes}
 }
 
 func (b *Buffer) ReadByte() (byte, error) {
-	panic("Please implement the ReadByte function")
+	if b.size == 0 {
+		return 0, fmt.Errorf("buffer empty")
+	}
+	res := b.buffer[b.readIndex]
+	b.size--
+	b.readIndex++
+	if b.readIndex == len(b.buffer) {
+		b.readIndex = 0
+	}
+
+	return res, nil
 }
 
 func (b *Buffer) WriteByte(c byte) error {
-	panic("Please implement the WriteByte function")
+	if b.size == len(b.buffer) {
+		return fmt.Errorf("buffer full")
+	} else {
+		b.buffer[b.writeIndex] = c
+		b.size++
+		b.writeIndex++
+		if b.writeIndex == len(b.buffer) {
+			b.writeIndex = 0
+		}
+		return nil
+	}
 }
 
 func (b *Buffer) Overwrite(c byte) {
-	panic("Please implement the Overwrite function")
+	if b.size < len(b.buffer) {
+		b.WriteByte(c)
+	} else {
+		b.buffer[b.writeIndex] = c
+		b.writeIndex++
+		if b.writeIndex == len(b.buffer) {
+			b.writeIndex = 0
+		}
+		b.readIndex++
+		if b.readIndex == len(b.buffer) {
+			b.readIndex = 0
+		}
+	}
 }
 
 func (b *Buffer) Reset() {
-	panic("Please implement the Reset function")
+	b.buffer = make([]byte, len(b.buffer))
+	b.readIndex, b.writeIndex, b.size = 0, 0, 0
 }
